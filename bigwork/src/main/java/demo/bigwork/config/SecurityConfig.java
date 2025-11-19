95608f7c3b6bdb253bd632eeb5ecfbe435ce2a3c
eric888fc <a35124851@gmail.com>
2025-11-19 17:27:51 +0800

    11/19

--- bigwork/src/main/java/demo/bigwork/config/SecurityConfig.java
+++ bigwork/src/main/java/demo/bigwork/config/SecurityConfig.java
@@ -63,8 +63,6 @@ public class SecurityConfig {
 	    // 我們在允許的列表中，
 	    // 加入 "http://localhost:5500" (Live Server 的預設位址)
 	    configuration.setAllowedOrigins(Arrays.asList(
-	        "http://localhost:5173", // (Vite，先留著)
-	        "http://localhost:3000", // (create-react-app，先留著)
 	        "http://127.0.0.1:5500", // (Live Server)
 	        "http://localhost:5500"  // (Live Server)
 	    ));
